8054119795fac4900057f2c43464b198b37adb91
Michal Kostenec <kostenec@civ.zcu.cz>
2014-01-30 14:37:33 +0100

    update-hosts: DUID, IAID supported is_iaid(), trim() added

--- Sauron/Util.pm
+++ Sauron/Util.pm
@@ -63,6 +63,8 @@ sub write2log{
 	     new_serial
 	     decode_daterange_str
 	     utimefmt
+         is_iaid
+         trim
 	    );
 
 
@@ -167,7 +169,7 @@ sub is_cidr($) {
 sub is_ip($) {
   my($ip) = @_;
 
-  return 1 if (is_cidr($ip) && $ip !~ /\/\d{1,2}$/);
+  return 1 if (is_cidr($ip) && $ip !~ /\/\d{1,3}$/);
   return 0;
 }
 
@@ -877,6 +879,27 @@ sub utimefmt($$) {
 
     return (defined($utime_df{$fmt}) ? $utime_df{$fmt}($utime) : $utime);
 }
+
+sub is_iaid($) {
+    my ($iaid) = @_;
+    $iaid =~ s/[^0-9A-F]//g;
+
+    if($iaid !~ /^\d+$/) {
+        $iaid = hex($iaid) if ($iaid !~ /^\d+$/ and $iaid ne "");
+    }
+
+    return ((($iaid > 0) and ($iaid < (2**32))) ? $iaid : 0); 
+}
+
+sub trim($) {
+
+    my ($s) = @_;
+    $s =~ s/^\s+//g;
+    $s =~ s/\s+$//g;
+
+    return $s;
+}
+
 
 1;
 # eof
