2eabd940ad654a2c963173506384a89d71ae2df8
Michal Kostenec <kostenec@civ.zcu.cz>
2014-05-19 14:01:34 +0200

    DB: SQL files updated

--- sql/DEFAULTS.sql
+++ sql/DEFAULTS.sql
@@ -5,7 +5,7 @@
 
 /* database version (do not change unless you know what you're doing!) */
 
-INSERT INTO settings (setting,value) VALUES('dbversion','1.4');
+INSERT INTO settings (setting,value) VALUES('dbversion','1.5');
 
 /* add BIND's built-in ACLs */
 INSERT INTO acls (server,name) VALUES(-1,'any');
